580e2a504559b98945051074d94aef2e5b8100a7
Gilles Dubuc <kouiskas@deviantart.com>
2013-05-08 21:41:17 +0200

    Adding example of how the address endpoint is used

--- btcfile.php
+++ btcfile.php
@@ -1,7 +1,7 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://api.btcfile.com/content');
+curl_setopt($ch, CURLOPT_URL, 'http://api.btcfile.dev/sell');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // For the sake of making this sample self-contained, this script uses itself as the uploaded file
@@ -29,11 +29,32 @@ print_r($data2);
 
 // If you want to read the information without updating anything, just pass the update_url
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('update_url' => $data['update_url'], //You must pass the update_url when you just want to read data
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('update_url' => $data['update_url'], // You must pass the update_url when you just want to read data
 										  ));
 
 $data3 = json_decode(curl_exec($ch), true);
 
 print_r($data3);
+
+// To get an address to pay to and price information
+
+curl_setopt($ch, CURLOPT_URL, 'http://api.btcfile.dev/buy');
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('download_url' => $data['download_url'], // You must pass the download_url when you want to obtain a new address
+										  ));
+
+$data4 = json_decode(curl_exec($ch), true);
+
+print_r($data4);
+
+/* To check if full payment has been sent to that address
+   If fully paid, the disposable download link will be returned in the JSON in the "paid_download_url" field, until it's used by the buyer
+   Please note that the paid download url can only be used once and can't reissued. */
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('address' => $data4['address'], // You must pass the address to check the paid state
+										  ));
+
+$data5 = json_decode(curl_exec($ch), true);
+
+print_r($data5);
 
 ?>
