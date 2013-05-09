1e371920f325588ec4ccb5cdf2c791aca145be94
Gilles Dubuc <kouiskas@deviantart.com>
2013-05-09 22:03:21 +0200

    Simplification of the API

--- btcfile.php
+++ btcfile.php
@@ -7,10 +7,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 // For the sake of making this sample self-contained, this script uses itself as the uploaded file
 curl_setopt($ch, CURLOPT_POSTFIELDS, array('file' => '@' . __FILE__, // The only mandatory field is having at least one file, everything below else is optional and has default values
 										   'price' => 6000000, // Price of the content, expressed in satoshis
-										   'miners_fee' => 600000, // Fee for the miners, expressed in satoshis. Minimum is 500000
-										   'btcfile_fee' => 600000, // Fee for btcfile, expressed in satoshis. No minimum. Up to you if you want btcfile to survive or go bankrupt :)
 										   'address' => '1vWb36Rzmqf9FfysuHCeAYs982iZwLJqm', // Address that will receive the value set in "price", should be the content owner
-										   'api_address' => '184Z3JvhopcXJhjUPua5wAWjMheGDLa9wV', // Address that will receive 10% of the value set in "btcfile_fee"
 										  ));
 
 $data = json_decode(curl_exec($ch), true);
